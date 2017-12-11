6ca8b5e67eec7de06d241b821de4693ca01f95bf
Kamsi Mbanefo <Kamsimbanefo@yahoo.com>
2017-12-11 09:08:31 +0000

    Previous Version of CDStore

---
+++ CDStore/CDStore/CDStoreDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace CDStore
+{
+    public class CDStoreDbContext : DbContext
+    {
+        public CDStoreDbContext() : base("myConnectionString")
+        {
+            Database.SetInitializer(new CustomInitializer());
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        
+    }
+
+}
